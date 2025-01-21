6ad6bd813bfed0ac8c4a52415223c7756687d10a
Bakhytzhan Leila <leilabakhytzhanova@gmail.com>
2025-01-21 17:33:04 +0500

    Lab1

---
+++ comments.py
@@ -0,0 +1,10 @@
+print("Hello,World!") #This is a comment
+#print("Hello,World!")
+#This is a comment
+#written in
+#more than just one line
+"""
+this is a comment 
+written in 
+more than just one line
+"""
